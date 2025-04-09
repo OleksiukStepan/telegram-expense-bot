e622dc5f6468f6654bc8a357701ad96161cf6eb5
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-04-09 17:25:23 +0300

    add schemas for model Article

---
+++ src/apps/expense_tracker/schemas.py
@@ -0,0 +1,28 @@
+from datetime import date
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class ArticleBase(BaseModel):
+    name: str
+    date: Optional[date] = None
+    amount: float
+
+
+class ArticleCreate(ArticleBase):
+    ...
+
+
+class ArticleRead(ArticleBase):
+    id: int
+    amount_usd: float
+
+    class Config:
+        orm_mode = True
+
+
+class ArticleUpdate(ArticleBase):
+    name: Optional[str] = None
+    date: Optional[date] = None
+    amount: Optional[float] = None
