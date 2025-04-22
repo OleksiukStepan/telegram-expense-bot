921579161824ac6785e4fd84667b293492beb693
Stepan Oleksiuk <161846860+OleksiukStepan@users.noreply.github.com>
2025-04-23 00:13:59 +0300

    update main.py

--- src/apps/telegram/main.py
+++ src/apps/telegram/main.py
@@ -1,10 +1,11 @@
 import asyncio
 from src.apps.telegram.bot import bot, dp
-from src.apps.telegram.handlers import start
+from src.apps.telegram.handlers import start, expense
 
 
 async def main():
     dp.include_router(start.router)
+    dp.include_router(expense.router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
